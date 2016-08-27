1c7d72196a30959893633ebe6a1bae070b4aebf5
Alexander Brown <DrJKL0424@gmail.com>
2016-08-27 09:26:27 -0700

    var to const/let

--- js/haas-chart.js
+++ js/haas-chart.js
@@ -2,10 +2,9 @@
 
 $j(document)
 	.ready(function () {
-		var valueOfUserSelectedColumn = function (questionId, rownumber) {
-			var idName;
-			for (var columnNumber = 1; columnNumber <= 4; columnNumber++) {
-				idName = '#QR\\~QID' + questionId + '\\~' + rownumber + '\\~' + columnNumber;
+		const valueOfUserSelectedColumn = function (questionId, rownumber) {
+			for (let columnNumber = 1; columnNumber <= 4; columnNumber++) {
+				let idName = '#QR\\~QID' + questionId + '\\~' + rownumber + '\\~' + columnNumber;
 				if ($j(idName)
 						.prop('checked')) {
 					return columnNumber - 1;
@@ -14,21 +13,21 @@ $j(document)
 			return 0;
 		};
 
-		var sumQuestion = function (questionId) {
-			var sum = 0;
-			for (var rowNumber = 1; rowNumber <= 4; rowNumber++) {
+		const sumQuestion = function (questionId) {
+			let sum = 0;
+			for (let rowNumber = 1; rowNumber <= 4; rowNumber++) {
 				sum += valueOfUserSelectedColumn(questionId, rowNumber);
 			}
 			return sum;
 		};
 
-		var activismQID = 24;
-		var communityEngagedQID = 27;
-		var directServiceQID = 28;
-		var philanthropyQID = 29;
-		var policyPoliticsQID = 30;
-		var socialEntrepreneurshipQID = 31;
-		var data = [{
+		const activismQID = 24;
+		const communityEngagedQID = 27;
+		const directServiceQID = 28;
+		const philanthropyQID = 29;
+		const policyPoliticsQID = 30;
+		const socialEntrepreneurshipQID = 31;
+		const data = [{
 			className: 'public-service-chart',
 			axes: [{
 				axis: 'Activism',
@@ -56,10 +55,10 @@ $j(document)
 			}]
 		}];
 
-		var experience = genericStarChart('Experience', [1, 1, 1, 1, 1, 1]);
-		var interest = genericStarChart('Interest', [4, 4, 4, 4, 4, 4]);
-		var strength = genericStarChart('Strength', [2, 2, 2, 2, 2, 2]);
-		var impact = genericStarChart('Impact', [3, 3, 3, 3, 3, 3]);
+		const experience = genericStarChart('Experience', [1, 1, 1, 1, 1, 1]);
+		const interest = genericStarChart('Interest', [4, 4, 4, 4, 4, 4]);
+		const strength = genericStarChart('Strength', [2, 2, 2, 2, 2, 2]);
+		const impact = genericStarChart('Impact', [3, 3, 3, 3, 3, 3]);
 
 		function genericStarChart (name, rowNumberArray) {
 			return [{
@@ -91,9 +90,9 @@ $j(document)
 			}];
 		}
 
-		var chart = RadarChart.chart();
-		var cfg = chart.config();// retrieve default config
-		var svg = d3.select('#haas-chart')
+		const chart = RadarChart.chart();
+		let cfg = chart.config();// retrieve default config
+		const svg = d3.select('#haas-chart')
 			.append('svg')
 			.attr('width', cfg.w)
 			.attr('height', cfg.h + cfg.h / 4);
@@ -116,7 +115,7 @@ $j(document)
 		cfg = chart.config();
 
 		function render () {
-			var game = svg.selectAll('g.game')
+			const game = svg.selectAll('g.game')
 				.data([interest, experience, strength, impact]);
 			game.enter()
 				.append('g')
