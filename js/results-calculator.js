d663db732a61bb565b4b13faeaa920282873dc92
Alexander Brown <DrJKL0424@gmail.com>
2016-08-28 09:03:54 -0700

    Tabs to Spaces?

--- js/results-calculator.js
+++ js/results-calculator.js
@@ -1,19 +1,19 @@
 const valueOfUserSelectedColumn = function (questionId, rownumber) {
-	let idName;
-	for (let columnNumber = 1; columnNumber <= 4; columnNumber++) {
-		idName = '#QR\\~QID' + questionId + '\\~' + rownumber + '\\~' + columnNumber;
-		if ($j(idName)
-				.prop('checked')) {
-			return columnNumber - 1;
-		}
-	}
-	return 0;
+    let idName;
+    for (let columnNumber = 1; columnNumber <= 4; columnNumber++) {
+        idName = '#QR\\~QID' + questionId + '\\~' + rownumber + '\\~' + columnNumber;
+        if ($j(idName)
+            .prop('checked')) {
+            return columnNumber - 1;
+        }
+    }
+    return 0;
 };
 
 const sumQuestion = function (questionId) {
-	let sum = 0;
-	for (let rowNumber = 1; rowNumber <= 4; rowNumber++) {
-		sum += valueOfUserSelectedColumn(questionId, rowNumber);
-	}
-	return sum;
+    let sum = 0;
+    for (let rowNumber = 1; rowNumber <= 4; rowNumber++) {
+        sum += valueOfUserSelectedColumn(questionId, rowNumber);
+    }
+    return sum;
 };
