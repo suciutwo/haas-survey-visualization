723ad87aff7371e08ab2397bd312fc5c4619b87b
Alexander Brown <DrJKL0424@gmail.com>
2016-08-29 11:03:02 -0700

    Add re-render button

--- js/haas-chart.js
+++ js/haas-chart.js
@@ -16,57 +16,60 @@ const genericStarChart = (name) => [{
 
 var svg;
 
-$j(document)
-	.ready(function () {
-		const data = [{
-			className: 'public-service-chart',
-			axes: categories.map(cat => cat.asAxis())
-		}];
+const renderPage = function () {
+	$j('#haas-chart').empty();
+	const data = [{
+		className: 'public-service-chart',
+		axes: categories.map(cat => cat.asAxis())
+	}];
 
-		const chart = RadarChart.chart();
-		let cfg = chart.config();// retrieve default config
-		svg = d3.select('#haas-chart')
-			.append('svg')
-			.attr('width', cfg.w)
-			.attr('height', cfg.h + cfg.h / 4);
-		chart.config({
-			maxValue: 12
-		});
-		svg.append('g')
-			.classed('single', 1)
-			.datum(data)
-			.call(chart);
-
-		chart.config({
-			w: cfg.w / 4,
-			h: cfg.h / 4,
-			levels: 0,
-			circles: false,
-			maxValue: 3
-		});
-		cfg = chart.config();
-
-		function render () {
-			const game = svg.selectAll('g.game')
-				.data(areas.map(area => area.toLowerCase()).map(area => genericStarChart(area)));
-			game.enter()
-				.append('g')
-				.classed('game', 1);
-			game.attr('transform',
-				function (d, i) {
-					return 'translate(' + (i * cfg.w) + ',' + (cfg.h * 4) + ')';
-				})
-				.call(chart);
-		}
-		render();
-
-		// Hide the survey response summary header and all of the questions so that only the chart displays
-		$j('#ResponseSummaryHeader')
-			.hide(); // header
-		$j('.QuestionOuter')
-			.hide(); // all questions
-
-		// Label the final button "Submit" instead of "Save & Continue"
-		$j('#NextButton')
-			.attr('value', 'Submit');
+	const chart = RadarChart.chart();
+	let cfg = chart.config();// retrieve default config
+	svg = d3.select('#haas-chart')
+		.append('svg')
+		.attr('width', cfg.w)
+		.attr('height', cfg.h + cfg.h / 4);
+	chart.config({
+		maxValue: 12
 	});
+	svg.append('g')
+		.classed('single', 1)
+		.datum(data)
+		.call(chart);
+
+	chart.config({
+		w: cfg.w / 4,
+		h: cfg.h / 4,
+		levels: 0,
+		circles: false,
+		maxValue: 3
+	});
+	cfg = chart.config();
+
+	function render () {
+		const game = svg.selectAll('g.game')
+			.data(areas.map(area => area.toLowerCase()).map(area => genericStarChart(area)));
+		game.enter()
+			.append('g')
+			.classed('game', 1);
+		game.attr('transform',
+			function (d, i) {
+				return 'translate(' + (i * cfg.w) + ',' + (cfg.h * 4) + ')';
+			})
+			.call(chart);
+	}
+	render();
+
+	// Hide the survey response summary header and all of the questions so that only the chart displays
+	$j('#ResponseSummaryHeader')
+		.hide(); // header
+	$j('.QuestionOuter')
+		.hide(); // all questions
+
+	// Label the final button "Submit" instead of "Save & Continue"
+	$j('#NextButton')
+		.attr('value', 'Submit');
+};
+
+$j(document)
+	.ready(renderPage);
