ee3eef8e934ea352f05e0ff04d30abe906c699b3
Alexander Brown <DrJKL0424@gmail.com>
2016-08-31 09:31:27 -0700

    Large Area charts

--- js/defs.js
+++ js/defs.js
@@ -1,6 +1,7 @@
-function Category (name, qID) {
+function Category (name, qID, abbr) {
 	this.name = name;
 	this.qID = qID;
+	this.abbr = abbr;
 	this.result = () => new CategoryResult(qID);
 	this.asAxis = () => ({
 		axis: this.name,
@@ -33,12 +34,12 @@ const areas = ['Experience', 'Strength', 'Impact', 'Interest'];
 var categories;
 
 const initCategories = () => categories = [
-	new Category('Community Organizing and Activism', 24),
-	new Category('Community Engaged Learning and Research', 27),
-	new Category('Direct Service', 28),
-	new Category('Philanthropy', 29),
-	new Category('Policy and Governance', 30),
-	new Category('Social Entrepreneurship and Corporate Social Responsibility', 31),
+	new Category('Community Organizing and Activism', 24, 'Activism'),
+	new Category('Community Engaged Learning and Research', 27, 'Learning'),
+	new Category('Direct Service', 28, 'Service'),
+	new Category('Philanthropy', 29, 'Philanthropy'),
+	new Category('Policy and Governance', 30, 'Policy'),
+	new Category('Social Entrepreneurship and Corporate Social Responsibility', 31, 'Entrepreneurship'),
 ];
 
 initCategories();
--- js/haas-chart.js
+++ js/haas-chart.js
@@ -1,71 +1,82 @@
 /* Here's the part I wrote. This is the part you would change. */
 
 const individualScore = (field) => {
-	let axes = categories.map((c) => ({
-		value: c.result()[field]
-	}));
-	axes[3].axis = field;
-	axes[3].yOffset = -2;
-	return axes;
+    let axes = categories.map((c) => ({
+        axis: c.abbr,
+        value: c.result()[field],
+    }));
+    // axes[3].axis = field; axes[3].yOffset = -2;
+    return axes;
 };
 
 const genericStarChart = (axes) => [
-	{
-		className: 'public-service-chart',
-		axes,
-	}
+    {
+        className: 'public-service-chart',
+        axes
+    }
 ];
 
 var svg;
 
-const renderPage = function () {
-	$j('#haas-chart').empty();
-	const data = genericStarChart(categories.map(cat => cat.asAxis()));
+const renderPage = function() {
+    $j('#haas-chart').empty();
+    const data = genericStarChart(categories.map(cat => cat.asAxis()));
 
-	const chart = RadarChart.chart();
-	let cfg = chart.config();// retrieve default config
-	svg = d3
-		.select('#haas-chart')
-		.append('svg')
-		.attr('width', cfg.w)
-		.attr('height', cfg.h + cfg.h / 4);
-	chart.config({maxValue: 12});
-	svg
-		.append('g')
-		.classed('single', 1)
-		.datum(data)
-		.call(chart);
+    const chart = RadarChart.chart();
+    let cfg = chart.config(); // retrieve default config
+    svg = d3
+        .select('#haas-chart')
+        .append('svg')
+        .attr('width', cfg.w + 200)
+        .attr('height', cfg.h + 200);
 
-	chart.config({
-		w: cfg.w / 4,
-		h: cfg.h / 4,
-		levels: 0,
-		circles: false,
-		maxValue: 3,
-	});
-	cfg = chart.config();
+    chart.config({
+        w: cfg.w / 2,
+        h: cfg.h / 2,
+        levels: 0,
+        circles: false,
+        maxValue: 3
+    });
+    cfg = chart.config();
 
-	function render () {
-		const areaData = areas.map(area => genericStarChart(individualScore(area.toLowerCase())));
-		const game = svg
-			.selectAll('g.game')
-			.data(areaData);
-		game
-			.enter()
-			.append('g')
-			.classed('game', 1);
-		game.attr('transform', function (d, i) {
-			return 'translate(' + (i * cfg.w) + ',' + (cfg.h * 4) + ')';
-		}).call(chart);
-	}
-	render();
+    function render() {
+        const areaData = areas.map(area => genericStarChart(individualScore(area.toLowerCase())));
+        const game = svg
+            .selectAll('g.game')
+            .data(areaData);
+        game
+            .enter()
+            .append('g')
+            .classed('game', 1);
+        const offset = 0;
+        const padding = 100;
+        const translations = [
+            [
+                offset, offset,
+            ],
+            [
+                cfg.w + padding,
+                offset,
+            ],
+            [
+                offset, cfg.h + padding,
+            ],
+            [
+                cfg.w + padding,
+                cfg.h + padding,
+            ],
+        ];
 
-	// Hide the survey response summary header and all of the questions so that only the chart displays
-	$j('#ResponseSummaryHeader').hide(); // header
-	$j('.QuestionOuter').hide(); // all questions
+        game.attr('transform', (d, i) => 'translate(' + translations[i][0] + ',' + translations[i][1] + ')';).call(chart);
+    }
+    render();
 
-	// Label the final button "Submit" instead of "Save & Continue"
-	$j('#NextButton').attr('value', 'Submit');
+    // Hide the survey response summary header and all of the questions so that only the chart displays
+    $j('#ResponseSummaryHeader').hide(); // header
+    $j('.QuestionOuter').hide(); // all questions
+
+    // Label the final button "Submit" instead of "Save & Continue"
+    $j('#NextButton').attr('value', 'Submit');
 };
 
 $j(document).ready(renderPage);
